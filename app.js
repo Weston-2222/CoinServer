04e4928f97abfcade1201f62678eb8f235d3394e
weston-2222 <westonxie22@gmail.com>
2024-09-24 16:48:29 +0800

    fix: 修改app.js

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 const coinInfoRoutes = require('./src/routes/coinInfoRoutes');
 const klineRoutes = require('./src/routes/klineRoutes');
 const cors = require('cors');
+const responseFormatter = require('./src/middleware/responseFormatter');
+const newsRoutes = require('./src/routes/newsRoutes');
 require('dotenv').config();
 const { testConnection } = require('./config/db');
 // 中間件
@@ -10,7 +12,7 @@ app.use(express.json());
 app.use(cors());
 app.use(responseFormatter);
 // 路由
-app.use('/api', coinInfoRoutes, klineRoutes);
+app.use('/api', coinInfoRoutes, klineRoutes, newsRoutes);
 
 // 啟動服務器
 const port = process.env.PORT || 8080;
