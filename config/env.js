bdc873c487bcb714f4b80cb9a6ac2c61aab6ea03
weston-2222 <westonxie22@gmail.com>
2024-10-22 18:15:02 +0800

    feat: 增加is_production
    is_production用來判斷是否在生產環境

--- config/env.js
+++ config/env.js
@@ -8,6 +8,9 @@ const jwt_config = {
   refresh_secret: process.env.JWT_SECRET_REFRESH_TOKEN,
 };
 
+const is_production = process.env.IS_PRODUCTION == 'true';
+
 module.exports = {
   jwt_config,
+  is_production,
 };
