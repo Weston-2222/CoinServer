6b843a24d0d982fd271ead19e4403b2fe8fa2dc0
weston-2222 <westonxie22@gmail.com>
2024-10-22 18:16:27 +0800

    feat: 新增token utils

--- src/utils/token.js
+++ src/utils/token.js
@@ -1,19 +1,37 @@
 const jwt = require('jsonwebtoken');
 const { jwt_config } = require('../../config/env');
+const { is_production } = require('../../config/env');
+const generateAccessToken = (params) => {
+  const payload = {
+    id: params.id,
+    email: params.email,
+  };
 
-const generateAccessToken = (payload) => {
   return jwt.sign(payload, jwt_config.access_secret, {
-    expiresIn: '60s',
+    expiresIn: '30m',
   });
 };
 
-const generateRefreshToken = (payload) => {
+const generateRefreshToken = (params) => {
+  const payload = {
+    id: params.id,
+    email: params.email,
+  };
   return jwt.sign(payload, jwt_config.refresh_secret, {
     expiresIn: '7d',
+  });
+};
+
+const setJwtToCookie = (kind, res, token, maxAge = 3600000) => {
+  res.cookie(kind, token, {
+    httpOnly: true,
+    secure: is_production,
+    maxAge: maxAge,
   });
 };
 
 module.exports = {
   generateAccessToken,
   generateRefreshToken,
+  setJwtToCookie,
 };
