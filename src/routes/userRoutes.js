43abd4021faa57e0db200f0fe48b9844a6d00b32
weston-2222 <westonxie22@gmail.com>
2024-10-22 18:17:00 +0800

    feat: user增加功能

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,8 +2,14 @@ const express = require('express');
 const router = express.Router();
 const User = require('../controllers').User;
 const bcrypt = require('bcrypt');
+const passport = require('passport');
+const {
+  generateAccessToken,
+  generateRefreshToken,
+  setJwtToCookie,
+} = require('../utils/token');
 //註冊
-router.post('/user', async (req, res) => {
+router.post('/auth/register', async (req, res) => {
   try {
     // 解構賦值
     const { email, password, name } = req.body;
@@ -17,16 +23,52 @@ router.post('/user', async (req, res) => {
     };
     // 註冊user
     const result = await User.register(user);
+
+    const access_token = generateAccessToken(result);
+
+    const refresh_token = generateRefreshToken(result);
+
+    setJwtToCookie('access_token', res, access_token);
+
+    setJwtToCookie('refresh_token', res, refresh_token);
+
+    await User.setRefreshTokenToUserDb(result.id, refresh_token);
+
+    delete result.id;
     res.sendFormattedJson(result);
   } catch (error) {
     res.status(500).json({ message: error.message, data: null });
   }
 });
 
-// //取得user資料
-// router.get('/user', User.register);
-
 // //更新user資料
-// router.patch('/user', User.register);
+router.patch(
+  '/user',
+  passport.authenticate('access', { session: false }),
+  async (req, res) => {
+    try {
+      const { name } = req.body;
 
+      const result = await User.patchUser(req.user.id, { name });
+
+      res.sendFormattedJson(result);
+    } catch (error) {
+      res.status(500).json({ message: error.message, data: null });
+    }
+  }
+);
+router.get(
+  '/user',
+  passport.authenticate('access', { session: false }),
+  (req, res) => {
+    const user = {
+      name: req.user.name,
+      email: req.user.email,
+      created_at: req.user.created_at,
+      updated_at: req.user.updated_at,
+      last_login: req.user.last_login,
+    };
+    res.sendFormattedJson(user);
+  }
+);
 module.exports = router;
